1c0a0b0c613b28553a67f44c18d785d5e3856c34
zhongytgo <2472935991@qq.com>
2022-04-05 21:27:21 +0800

    4/5 21:27_zyt

--- src/views/user/course/coursecenter.vue
+++ src/views/user/course/coursecenter.vue
@@ -1,15 +1,223 @@
 <template>
 <div>
-  <h1>hhh</h1>
+  <div class="container-fluid custom-container">
+    <div class="row">
+      <div class="col-xl-12">
+        <div class=" full-width">
+          <div class="row">
+            <div class="col-xl-3 col-lg-6 col-md-6">
+              <div class="sa-causes-single sa-causes-single-2">
+                <div class="entry-thumb">
+                  <img src="../../../assets/img/course.jpg" alt="img" class="img-fluid full-width">
+                  <div class="dontaion-category story-meta">
+                    <p><strong>Michle Colen</strong></p>
+                    <p>Goal: <strong>$4585.00</strong></p>
+                  </div>
+                </div>
+                <div class="causes-details-wrap">
+                  <div class="causes-details">
+                    <h5><a href="story-details.html">More than One Life Changed</a></h5>
+                    <p>Many desktop publishing package and the web page editor now use lorem Ipsum the model text lorem.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6">
+              <div class="sa-causes-single sa-causes-single-2">
+                <div class="entry-thumb">
+                  <img src="../../../assets/img/course.jpg" alt="img" class="img-fluid full-width">
+                  <div class="dontaion-category story-meta">
+                    <p><strong>Michle Colen</strong></p>
+                    <p>Goal: <strong>$4585.00</strong></p>
+                  </div>
+                </div>
+                <div class="causes-details-wrap">
+                  <div class="causes-details">
+                    <h5><a href="story-details.html">Help for Children of the East</a></h5>
+                    <p>Many desktop publishing package and the web page editor now use lorem Ipsum the model text lorem.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6">
+              <div class="sa-causes-single sa-causes-single-2">
+                <div class="entry-thumb">
+                  <img src="../../../assets/img/course.jpg" alt="img" class="img-fluid full-width">
+                  <div class="dontaion-category story-meta">
+                    <p><strong>Michle Colen</strong></p>
+                    <p>Goal: <strong>$4585.00</strong></p>
+                  </div>
+                </div>
+                <div class="causes-details-wrap">
+                  <div class="causes-details">
+                    <h5><a href="story-details.html">Help for Children of the East</a></h5>
+                    <p>Many desktop publishing package and the web page editor now use lorem Ipsum the model text lorem.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6">
+              <div class="sa-causes-single sa-causes-single-2">
+                <div class="entry-thumb">
+                  <img src="../../../assets/img/course.jpg" alt="img" class="img-fluid full-width">
+                  <div class="dontaion-category story-meta">
+                    <p><strong>Michle Colen</strong></p>
+                    <p>Goal: <strong>$4585.00</strong></p>
+                  </div>
+                </div>
+                <div class="causes-details-wrap">
+                  <div class="causes-details">
+                    <h5><a href="story-details.html">More than One Life Changed</a></h5>
+                    <p>Many desktop publishing package and the web page editor now use lorem Ipsum the model text lorem.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6">
+              <div class="sa-causes-single sa-causes-single-2">
+                <div class="entry-thumb">
+                  <img src="../../../assets/img/course.jpg" alt="img" class="img-fluid full-width">
+                  <div class="dontaion-category story-meta">
+                    <p><strong>Michle Colen</strong></p>
+                    <p>Goal: <strong>$4585.00</strong></p>
+                  </div>
+                </div>
+                <div class="causes-details-wrap">
+                  <div class="causes-details">
+                    <h5><a href="story-details.html">More than One Life Changed</a></h5>
+                    <p>Many desktop publishing package and the web page editor now use lorem Ipsum the model text lorem.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6">
+              <div class="sa-causes-single sa-causes-single-2">
+                <div class="entry-thumb">
+                  <img src="../../../assets/img/course.jpg" alt="img" class="img-fluid full-width">
+                  <div class="dontaion-category story-meta">
+                    <p><strong>Michle Colen</strong></p>
+                    <p>Goal: <strong>$4585.00</strong></p>
+                  </div>
+                </div>
+                <div class="causes-details-wrap">
+                  <div class="causes-details">
+                    <h5><a href="story-details.html">Help for Children of the East</a></h5>
+                    <p>Many desktop publishing package and the web page editor now use lorem Ipsum the model text lorem.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6">
+              <div class="sa-causes-single sa-causes-single-2">
+                <div class="entry-thumb">
+                  <img src="../../../assets/img/course.jpg" alt="img" class="img-fluid full-width">
+                  <div class="dontaion-category story-meta">
+                    <p><strong>Michle Colen</strong></p>
+                    <p>Goal: <strong>$4585.00</strong></p>
+                  </div>
+                </div>
+                <div class="causes-details-wrap">
+                  <div class="causes-details">
+                    <h5><a href="story-details.html">More than One Life Changed</a></h5>
+                    <p>Many desktop publishing package and the web page editor now use lorem Ipsum the model text lorem.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-md-6">
+              <div class="sa-causes-single sa-causes-single-2">
+                <div class="entry-thumb">
+                  <img src="../../../assets/img/course.jpg" alt="img" class="img-fluid full-width">
+                  <div class="dontaion-category story-meta">
+                    <p><strong>Michle Colen</strong></p>
+                    <p>Raised: <strong>$4585.00</strong></p>
+                  </div>
+                </div>
+                <div class="causes-details-wrap">
+                  <div class="causes-details">
+                    <h5><a href="story-details.html">More than One Life Changed</a></h5>
+                    <p>Many desktop publishing package and the web page editor now use lorem Ipsum the model text lorem.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+<!--        <div class="custom-pagination align-item-center">-->
+<!--          <nav aria-label="Page navigation example">-->
+<!--            <ul class="pagination">-->
+<!--              <li class="page-item">-->
+<!--                <a class="page-link" href="#" aria-label="Previous"> <span aria-hidden="true">«</span>-->
+<!--                  <span class="sr-only">Previous</span>-->
+<!--                </a>-->
+<!--              </li>-->
+<!--              <li class="page-item"><a class="page-link" href="#">1</a>-->
+<!--              </li>-->
+<!--              <li class="page-item active"><a class="page-link" href="#">2</a>-->
+<!--              </li>-->
+<!--              <li class="page-item"><a class="page-link" href="#">3</a>-->
+<!--              </li>-->
+<!--              <li class="page-item">-->
+<!--                <a class="page-link" href="#" aria-label="Next"> <span aria-hidden="true">»</span>-->
+<!--                  <span class="sr-only">Next</span>-->
+<!--                </a>-->
+<!--              </li>-->
+<!--            </ul>-->
+<!--          </nav>-->
+<!--        </div>-->
+      </div>
+    </div>
+  </div>
+  <tail style="margin-top: 25px"></tail>
 </div>
 </template>
 
 <script>
+import tail from "../tail.vue";
 export default {
-  name: "coursecenter"
+  name: "coursecenter",
+  components: {
+    tail,
+  },
+
 }
 </script>
 
 <style scoped>
-
+.container-fluid {
+  /*width: 100%;*/
+  padding-top: 40px;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto
+}
+.custom-container {
+  padding-right: 8%;
+  padding-left: 8%;
+}
+.row {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px
+}
+.col-xl-3{
+  position: relative;
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 15px;
+  flex: 0 0 25%;
+}
+.sa-causes-single {
+  box-shadow: 0 0 0 1px rgb(67 41 163 / 8%), 0 1px 5px 0 rgb(67 41 163 / 8%);
+  margin-bottom: 30px;
+  border-radius: 4px;
+  transition: 0.3s;
+}
+.entry-thumb img{
+  width: 100%;
+}
 </style>
